c503004e6495a12c2036efe0a9dc6c4673723a36
Aakash <aakash@Srilekhas-MacBook-Air.local>
2023-08-29 20:33:34 -0700

    updated row display

--- src/Components/CloudscapeTable/CloudscapeTable.tsx
+++ src/Components/CloudscapeTable/CloudscapeTable.tsx
@@ -29,25 +29,27 @@ import { useCollection } from "@cloudscape-design/collection-hooks";
 import * as React from "react";
 import { useEffect } from "react";
 import { IInputs } from "../../generated/ManifestTypes";
+import * as moment from "moment-timezone";
+import { DefaultDateFormat, DefaultDateTimeFormat } from "./CellComponents";
 
 export interface CloudscapeTableProps {
   kpiEntityId: string;
   kpiEntityName: string;
   pcfContext: ComponentFramework.Context<IInputs>;
-  itemsPerPage : number;
+  itemsPerPage: number;
 }
 const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
   kpiEntityId,
   kpiEntityName,
   pcfContext,
-  itemsPerPage
+  itemsPerPage,
 }) => {
   const [dataLoading, setDataLoading] = React.useState(false);
   const [dataLoadingStatus, setDataLoadingStatus] = React.useState<
     "loading" | "error" | "success"
   >("loading");
 
-  const [primaryEntity, setPrimaryEntityName] = React.useState('');
+  const [primaryEntity, setPrimaryEntityName] = React.useState("");
   const [allColumns, setAllColumns] = React.useState<
     DynamicColumnDetails | undefined
   >();
@@ -83,7 +85,11 @@ const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
   // generating Table column definitions from allColumns
   useEffect(() => {
     if (allColumns) {
-      const columnDefinitions = generateColumnDefinitions(allColumns, pcfContext, primaryEntity);
+      const columnDefinitions = generateColumnDefinitions(
+        allColumns,
+        pcfContext,
+        primaryEntity
+      );
       console.log("columnDefinitions ", columnDefinitions);
       setTableColumnDefinitions(columnDefinitions);
     }
@@ -112,9 +118,12 @@ const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
     try {
       if (kpiEntityId) {
         var fetchData = {
-          "cb_kpimasterdataid" : kpiEntityId
+          cb_kpimasterdataid: kpiEntityId,
         };
-        console.log("fetchData.cb_kpimasterdataid",fetchData.cb_kpimasterdataid)
+        console.log(
+          "fetchData.cb_kpimasterdataid",
+          fetchData.cb_kpimasterdataid
+        );
         var fetchXml = [
           "<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>",
           "  <entity name='cb_kpimasterdata'>",
@@ -124,100 +133,76 @@ const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
           "    <attribute name='cb_columnlayout'/>",
           "    <order attribute='cb_name' descending='false'/>",
           "    <filter type='and'>",
-          "      <condition attribute='cb_kpimasterdataid' operator='eq' value='", kpiEntityId/*{2ED4C990-9C42-EE11-BDF4-0022482A939E}*/, "'/>",
+          "      <condition attribute='cb_kpimasterdataid' operator='eq' value='",
+          kpiEntityId /*{2ED4C990-9C42-EE11-BDF4-0022482A939E}*/,
+          "'/>",
           "    </filter>",
           "  </entity>",
-          "</fetch>"
-          ].join("");
+          "</fetch>",
+        ].join("");
 
-        pcfContext.webAPI.retrieveMultipleRecords("cb_kpimasterdata", "?fetchXml=" + encodeURIComponent(fetchXml)).then(
-          (results: any) => {
-            if (results && results.entities && results.entities.length > 0) {
+        pcfContext.webAPI
+          .retrieveMultipleRecords(
+            "cb_kpimasterdata",
+            "?fetchXml=" + encodeURIComponent(fetchXml)
+          )
+          .then(
+            (results: any) => {
+              if (results && results.entities && results.entities.length > 0) {
+                var firstKpiEntity = results.entities;
+                console.log("firstKpiEntity ", firstKpiEntity);
 
-              var firstKpiEntity = results.entities;
-              console.log("firstKpiEntity ",  firstKpiEntity);
+                let cb_fetchxml = firstKpiEntity[0].cb_fetchxml;
+                console.log("cb_fetchxml ", cb_fetchxml);
 
-              let cb_fetchxml = firstKpiEntity[0].cb_fetchxml;
-              console.log("cb_fetchxml ", cb_fetchxml);
+                let _columnlayout = firstKpiEntity[0].cb_columnlayout;
+                console.log("cb_columnlayout ", JSON.stringify(_columnlayout));
 
-              let _columnlayout = firstKpiEntity[0].cb_columnlayout;
-              console.log("cb_columnlayout ", JSON.stringify(_columnlayout));
+                let _finalColumnLayout =
+                  _columnlayout != null ? JSON.parse(_columnlayout) : null;
 
-              let _finalColumnLayout = _columnlayout != null ? JSON.parse(_columnlayout) : null;
+                let fetchXML_AllItems = cb_fetchxml.replace(/"/g, "'");
+                console.log("fetchXML_AllItems : ", fetchXML_AllItems);
 
-              let fetchXML_AllItems = cb_fetchxml.replace(/"/g, "'");
-              console.log("fetchXML_AllItems : ",fetchXML_AllItems);
+                const _primaryEntityName =
+                  getPrimaryEntityNameFromFetchXml(fetchXML_AllItems);
+                console.log("primaryEntityName ", _primaryEntityName);
 
-              const _primaryEntityName = getPrimaryEntityNameFromFetchXml(fetchXML_AllItems);
-              console.log("primaryEntityName ", _primaryEntityName);
-              
-              setPrimaryEntityName(_primaryEntityName);
-              setAllColumns(_finalColumnLayout);
+                setPrimaryEntityName(_primaryEntityName);
+                setAllColumns(_finalColumnLayout);
 
-              pcfContext.webAPI.retrieveMultipleRecords(_primaryEntityName, "?fetchXml=" + encodeURIComponent(fetchXML_AllItems)).then(
-                (results: any) => {
-                  if (results && results.entities && results.entities.length > 0) {
-                    const _allItems = results.entities;
-                    console.log("_allItems ", _allItems);
-                    setAllItems(_allItems);
-                    setDataLoadingStatus("success");
-                  }
-              },
-              (e: any) => {
-                  console.error("An error occurred:", e);
-                  setDataLoadingStatus("error");
-              });
-            }
+                pcfContext.webAPI
+                  .retrieveMultipleRecords(
+                    _primaryEntityName,
+                    "?fetchXml=" + encodeURIComponent(fetchXML_AllItems)
+                  )
+                  .then(
+                    (results: any) => {
+                      if (
+                        results &&
+                        results.entities &&
+                        results.entities.length > 0
+                      ) {
+                        const _allItems = results.entities;
+                        console.log("_allItems ", _allItems);
+                        setAllItems(_allItems);
+                        setDataLoadingStatus("success");
+                      }
+                    },
+                    (e: any) => {
+                      console.error("An error occurred:", e);
+                      setDataLoadingStatus("error");
+                    }
+                  );
+              }
             },
-      
-        (e: any) => {
-            console.error("An error occurred:", e);
-            setDataLoadingStatus("error");
-        });
-      
-      }
 
-
-
-     /*   const kpiResult = await pcfContext.webAPI.retrieveMultipleRecords(
-          "cb_kpimasterdata",
-          `?fetchXml=${encodeURIComponent(KPI_FETCH_XML)}`
-        );
-        console.log("kpiResult ", JSON.stringify(kpiResult));
-
-        if (kpiResult.entities && kpiResult.entities.length > 0) {
-          const firstKpiEntity = kpiResult.entities[0].value; // TODO - check once to make sure it is values
-          console.log("firstKpiEntity ", JSON.stringify(firstKpiEntity));
-
-          const cb_fetchxml = firstKpiEntity.cb_fetchxml;
-          const cb_columnlayout = firstKpiEntity.cb_columnlayout;
-          setAllColumns(cb_columnlayout);
-
-          const _primaryEntityName = getPrimaryEntityNameFromFetchXml(cb_fetchxml);
-          setPrimaryEntityName(_primaryEntityName);
-          console.log("primaryEntityName ", _primaryEntityName);
-
-          const secondResult = await pcfContext.webAPI.retrieveMultipleRecords(
-            _primaryEntityName,
-            `?fetchXml=${encodeURIComponent(cb_fetchxml)}`
+            (e: any) => {
+              console.error("An error occurred:", e);
+              setDataLoadingStatus("error");
+            }
           );
-
-          console.log("Second API call result", secondResult);
-
-          if (secondResult && secondResult.entities && secondResult.entities.length > 0) {
-            const _allItems = secondResult.entities;
-            console.log("_allItems ", _allItems);
-
-            setAllItems(_allItems);
-          }
-        } else {
-          throw new Error("No KPI entities found.");
-        }
-      } else {
-        throw new Error("Invalid KPI Entity ID.");
       }
-
-      setDataLoadingStatus("success"); */
     } catch (error) {
       console.error("An error occurred:", error);
       setDataLoadingStatus("error");
@@ -235,6 +220,49 @@ const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
     }
     return primaryEntityName;
   };
+
+  function modifyRowData(
+    rowData: any[],
+    allColumns: DynamicColumnDetails
+  ): any[] {
+    const modifiedData = rowData.map((row) => {
+      const modifiedRow = { ...row };
+
+      allColumns.data.forEach((dataEntity) => {
+        if (dataEntity.isColumnVisible && dataEntity.fieldName in row) {
+          if (dataEntity.metadata.type === "date") {
+            const originalDate = row[dataEntity.fieldName];
+            if (originalDate) {
+              modifiedRow[dataEntity.fieldName] = moment(originalDate).format(
+                dataEntity.metadata.dateFormat || DefaultDateFormat
+              );
+            }
+          }
+
+          if (dataEntity.metadata.type === "dateTime") {
+            const originalDate = row[dataEntity.fieldName];
+            if (originalDate) {
+              modifiedRow[dataEntity.fieldName] = moment(originalDate).format(
+                dataEntity.metadata.dateFormat || DefaultDateTimeFormat
+              );
+            }
+          }
+
+          if (dataEntity.metadata.type === "boolean") {
+            const originalData = row[dataEntity.fieldName];
+            modifiedRow[dataEntity.fieldName] = originalData ? "Yes" : "No";
+          }
+
+          // Add more conditions for other data types if needed
+        }
+      });
+
+      return modifiedRow;
+    });
+
+    console.log("modifyRowData ", JSON.stringify(modifiedData));
+    return modifiedData;
+  }
 
   const {
     items,
@@ -293,7 +321,11 @@ const CloudscapeTable: React.FC<CloudscapeTableProps> = ({
           wrapLines={tableDefaultPreferences.wrapLines}
           stripedRows={tableDefaultPreferences.stripedRows}
           contentDensity={tableDefaultPreferences.contentDensity}
-          header={<Header counter={`(${tableRowData?.length})`}>{allColumns?.columnInfo.tableName || ''}</Header>}
+          header={
+            <Header counter={`(${tableRowData?.length})`}>
+              {allColumns?.columnInfo.tableName || ""}
+            </Header>
+          }
           filter={
             <PropertyFilter
               i18nStrings={propertyFilterI18nStrings("Table")}
